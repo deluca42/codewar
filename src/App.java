ee87b7f7e73fc577173f73377b0d09d3780a87a2
deluca42@gmail.com <*97178@git>
2022-09-20 14:14:29 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int i=1; i < 100 ; i++){
+            if (i >= 2 && i <= 5){  
+                if( i == 3){
+                    System.out.print("Wierd");
+                  
+                }
+                System.out.print("Not Weird");
+                
+            }
+    
+    }
+    }
+}
---
+++ src/stdinone/stdin.java
@@ -0,0 +1,18 @@
+package stdinone;
+
+import java.util.*;
+
+public class stdin {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+   
+        for(int i=0; i<3; i++){
+          int a = scan.nextInt();
+          System.out.println(a);
+       
+        }
+    }
+
+}
+
