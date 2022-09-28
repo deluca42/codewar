af8fe35f9cdbd5bc49969c5641948747c31a156b
deluca42@gmail.com <*97178@git>
2022-09-28 11:23:13 -0300

    add solve code

--- src/hackerrank/introduction/convertstring/ConvertString.java
+++ src/hackerrank/introduction/convertstring/ConvertString.java
@@ -2,4 +2,10 @@ package hackerrank.introduction.convertstring;
 
 public class ConvertString {
     
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    String str = String.valueOf(n);
+    System.out.println("Good job");
+    
+    
 }
